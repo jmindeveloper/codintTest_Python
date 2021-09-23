fe3a46eeb46490f41a3eaa8b4014ce859cf95491
jmindevelper <jmin.developer@gmail.com>
2021-09-23 23:44:45 +0900

    update

---
+++ 기초100제/기초-입출력.py
@@ -0,0 +1,66 @@
+# # 6009
+
+# i = input()
+# print(i)
+
+# # 6010
+
+# i = int(input())
+# print(i)
+
+# # 6011
+
+# print(float(input()))
+
+# # 6012
+
+# a = input()
+# b = input()
+# print(a)
+# print(b)
+
+# # 6013
+
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+
+# # 6014
+
+# i = input()
+# print(i)
+# print(i)
+# print(i)
+
+# # 6015
+
+# a, b = input().split()
+# print(a)
+# print(b)
+
+# # 6016
+
+# a, b = input().split()
+# print(b, a)
+
+# # 6017
+
+# i = input()
+# print(i, i, i)
+
+# # 6018
+
+# print(input())
+
+# # 6019
+
+# y, m, d = input().split('.')
+
+# print(f'{d}-{m}-{y}')
+
+# 6020
+
+a, b = input().split('-')
+print(a, b)
